f2a6c837e9eb2402129770830527158756a9e3e7
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2024-07-19 15:02:29 -0700

    added comments - breadth first algorithm

--- problems/01-breadth-first-traversal.js
+++ problems/01-breadth-first-traversal.js
@@ -3,6 +3,15 @@ Write a function called printBreadthFirst that will traverse the given graph
 breadth-first, printing each node when visited exactly once, on a newline.
 */
 
+// Breadth-first traversal algorithm
+
+// 1. Create a queue and enqueue the starting node
+// 2. Create a set to store visited nodes
+// 3. While the queue is not empty, repeat steps 4-6
+// 4. Dequeue the first node
+// 5. DO THE THING THAT YOU NEED TO FOR THE DEQUEUED NODE
+// 6. For each unvisited neighbor, add it to the visited nodes and to the back of the queue
+
 const adjList = {
   1: [2, 5],
   2: [1, 3, 5],
@@ -16,16 +25,16 @@ function printBreadthFirst(start) {
     // step 2 - create a set to store visited nodes
     const visited = new Set();
     visited.add(start);
-    // step 1 - create a queue for breadth first traversal
+    // step 1 - create a queue for breadth first traversal & enqueue starting node
     let q = [start];
 
-    // while there are nodes in the queue...
+    // step 3 - while there are nodes in the queue...
     while(q.length) {
-        // step 3 - shift off the first node
+        // step 4 - dequeue (shift off) the first node
         let node = q.shift();
-        // DO THE THING
+        // step 5 - DO THE THING
         console.log(node);
-        // Add neighbors to the queue using adjacency list variable /\
+        // step 6 - Add neighbors to the queue using adjacency list variable /\
         for(let neighbor of adjList[node]) {  // [node] keys into the node we're on, w/ values = neighbor nodes
             if(!visited.has(neighbor)) {    // if we haven't visited the next node yet,
                 visited.add(neighbor);        // add it to the visited set
